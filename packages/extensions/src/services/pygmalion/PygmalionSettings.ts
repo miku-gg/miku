8b8ec3cc310af47c9a0dad2f221e1ad904d00742
miku448 <iwantofftheride@yahoo.com>
2023-03-06 23:08:08 -0300

    Fix pygmalion settings

--- packages/extensions/src/services/pygmalion/PygmalionSettings.ts
+++ packages/extensions/src/services/pygmalion/PygmalionSettings.ts
@@ -1,42 +1,18 @@
 import _Pygmalion6B from './pygmalion-settings/Pygmalion-6B.json';
 
 export interface PygmalionSettings {
-	"file_version": number,
+	"max_context_length": number,
 	"max_length": number,
-	"ikmax": number,
-	"genamt": number,
-	"ikgen": number,
 	"rep_pen": number,
-	"rep_pen_slope": number,
 	"rep_pen_range": number,
-	"temp": number,
-	"top_p": number,
-	"top_k": number,
-	"top_a": number,
+	"rep_pen_slope": number,
+	"temperature": number,
 	"tfs": number,
+	"top_a": number,
+	"top_k": number,
+	"top_p": number,
 	"typical": number,
-	"numseqs": number,
-	"fp32_model": boolean,
-	"modeldim": number,
 	"sampler_order": number[]
-	"newlinemode": string,
-	"lazy_load": boolean,
-	"revision": null,
-	"selected_preset": string,
-	"horde_wait_time": number,
-	"horde_queue_position": number,
-	"horde_queue_size": number,
-	"model": string,
-	"model_type": string,
-	"url": string,
-	"oaiurl": string,
-	"oaiengines": string,
-	"colaburl": string,
-	"apikey": string,
-	"oaiapikey": string,
-	"configname": string,
-	"online_model": string,
-	"alt_multi_gen": boolean
 }
 
 export const pygmalionModels = ['pygmalion-6b'];
