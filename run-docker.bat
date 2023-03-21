85d3fe19658aae38ee082d98bcb91cf46f62efb9
mikudev <iwantofftheride@yahoo.com>
2023-03-21 16:56:02 -0300

    Improve windows install bat, remove docker

--- run-docker.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-docker build -t mikugg-app .
-docker run -p 8484:8484 -p 8585:8585 5173:5173 mikugg-app
--- run-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t mikugg-app .
-docker run -p 8484:8484 -p 8585:8585 5173:5173 mikugg-app
