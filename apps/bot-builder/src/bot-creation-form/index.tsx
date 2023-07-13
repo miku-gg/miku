6c0ff3d688b77913162e349481354485288b13e7
Mikudev <iwantofftheride@yahoo.com>
2023-07-13 12:20:26 +0200

    Skip sentence embedder deploys, bot builder link to docs

--- apps/bot-builder/src/bot-creation-form/index.tsx
+++ apps/bot-builder/src/bot-creation-form/index.tsx
@@ -5,7 +5,7 @@ import {
   useCharacterCreationForm,
 } from "./CharacterCreationFormContext";
 
-import { Button, TextHeading } from "@mikugg/ui-kit";
+import { Button, TextHeading, Tooltip } from "@mikugg/ui-kit";
 import { MikuCard } from "@mikugg/bot-utils";
 
 import { validateStep } from "./libs/CharacterData";
@@ -26,6 +26,8 @@ import saveIcon from "./assets/save.svg";
 import "./styles/main.scss";
 import { downloadPng, BUILDING_STEPS } from "./libs/encodePNG";
 import BotImport from "./Components/BotImport";
+
+const DocIcon = () => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="18" height="18"><path d="M3 2.75A2.75 2.75 0 0 1 5.75 0h14.5a.75.75 0 0 1 .75.75v20.5a.75.75 0 0 1-.75.75h-6a.75.75 0 0 1 0-1.5h5.25v-4H6A1.5 1.5 0 0 0 4.5 18v.75c0 .716.43 1.334 1.05 1.605a.75.75 0 0 1-.6 1.374A3.251 3.251 0 0 1 3 18.75ZM19.5 1.5H5.75c-.69 0-1.25.56-1.25 1.25v12.651A2.989 2.989 0 0 1 6 15h13.5Z"></path><path d="M7 18.25a.25.25 0 0 1 .25-.25h5a.25.25 0 0 1 .25.25v5.01a.25.25 0 0 1-.397.201l-2.206-1.604a.25.25 0 0 0-.294 0L7.397 23.46a.25.25 0 0 1-.397-.2v-5.01Z"></path></svg>;
 
 const save = (card: MikuCard) => {
   const cardJSON = JSON.stringify(card, null, 2);
@@ -74,7 +76,24 @@ const _CharacterCreationForm: React.FC = () => {
   return (
     <div className="characterCreationForm">
       <div className="characterCreationForm__headingContainer">
-        <TextHeading size="h1">Create a Character</TextHeading>
+        <div className="characterCreationForm__titleContainer">
+          <TextHeading size="h1">
+            Create a Character
+          </TextHeading>
+          <a
+            href="https://docs.miku.gg/guides/bots/create-bots/" target="_blank" rel="nofollow"
+            className="Input__infoIcon"
+            data-tooltip-id={`input-tooltip-documentation`}
+            data-tooltip-content="How to use this builder?"
+            data-tooltip-varaint="dark"
+          >
+            <DocIcon />
+          </a>
+          <Tooltip
+            id={`input-tooltip-documentation`}
+            place="bottom"
+          />
+        </div>
         <TopBar
           steps={[
             { label: "01", description: "Description" },
