24e9a2d80de2d3c7cf203a42b0eac8044518b06d
mikudev <iwantofftheride@yahoo.com>
2023-09-09 23:28:15 -0300

    Remove aphrodite from endpoints

--- apps/services-server/src/app.ts
+++ apps/services-server/src/app.ts
@@ -4,8 +4,6 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import multer from 'multer';
 import * as MikuExtensions from '@mikugg/extensions';
-import jwt from 'jsonwebtoken';
-import { IncomingHttpHeaders } from 'http2';
 
 // Load environment variables
 const path = require('path');
@@ -18,11 +16,6 @@ const NOVELAI_API_KEY = process.env.NOVELAI_API_KEY || '';
 const PYGMALION_ENDPOINT = process.env.PYGMALION_ENDPOINT || '';
 const OOBABOOGA_ENDPOINT = process.env.OOBABOOGA_ENDPOINT || '';
 const EMOTIONS_ENDPOINT = process.env.EMOTIONS_ENDPOINT || '';
-const APHRODITE_ENDPOINT = process.env.APHRODITE_ENDPOINT || '';
-const APHRODITE_S3_BUCKET = process.env.APHRODITE_S3_BUCKET || '';
-const APHRODITE_S3_REGION = process.env.APHRODITE_S3_REGION || '';
-const APHRODITE_S3_ACCESS_KEY = process.env.APHRODITE_S3_ACCESS_KEY || '';
-const APHRODITE_S3_SECRET_KEY = process.env.APHRODITE_S3_SECRET_KEY || '';
 const SBERT_EMOTIONS_ENABLED = Number(process.env.SBERT_EMOTIONS_ENABLED || '0');
 const SBERT_SIMILARITY_API_URL = process.env.SBERT_SIMILARITY_API_URL || '';
 const SBERT_SIMILARITY_API_TOKEN = '';
@@ -47,43 +40,8 @@ app.post('/audio-upload', uploadAudio.single('file'), (req, res) => {
   res.status(200).send(req.file?.filename || '');
 });
 
-const verifyJWT = async (headers: IncomingHttpHeaders): Promise<'EXPIRED' | 'REGULAR' | 'PREMIUM'> => {
-  return new Promise((resolve) => {
-    const authCookie = headers.cookie?.split(';').find((cookie: string) => cookie.includes('Authentication'));
-    const bearerToken = authCookie?.split('=')[1] || '';
-
-    if (bearerToken) {
-      console.log(bearerToken);
-      jwt.verify(bearerToken, process.env.JWT_SECRET || '', (err, _decodedData) => {
-        const decodedData = _decodedData as jwt.JwtPayload;
-        console.log(err);
-        if (err) {
-          resolve('EXPIRED');
-        } else if ((decodedData.exp || 0) < Date.now() / 1000) {
-          resolve('EXPIRED');
-        } else {
-          resolve(decodedData.isPremium ? 'PREMIUM' : 'REGULAR');
-        }
-      });
-    } else {
-      resolve('EXPIRED');
-    }
-  });
-};
-
-
 const addRoute = (path: string, cb: (body: any) => Promise<{ status: number, response: any}>) => {
   app.post(path, async (req, res) => {
-    const jwt = await verifyJWT(req.headers);
-    console.log('path', path, jwt);
-    if (path === `/${MikuExtensions.Services.ServicesNames.Aphrodite}/query` && jwt === 'EXPIRED') {
-      res.status(401).send('Unauthorized');
-      return;
-    }
-    if (path === `/${MikuExtensions.Services.ServicesNames.AzureTTS}/query` && jwt !== 'PREMIUM') {
-      res.status(401).send('Unauthorized');
-      return;
-    }
     const result = await cb(req.body);
     res.status(result.status).send(result.response);
   });
@@ -175,32 +133,6 @@ if (SBERT_EMOTIONS_ENABLED) {
     sbertSimilarityAPIUrl: SBERT_SIMILARITY_API_URL,
     addRoute
   });
-}
-
-if (APHRODITE_ENDPOINT) {
-  new MikuExtensions.Services.AphroditePromptCompleterService({
-    serviceId: MikuExtensions.Services.ServicesNames.Aphrodite,
-    billingEndpoint: '',
-    addRoute,
-    s3Bucket: APHRODITE_S3_BUCKET,
-    s3Config: {
-      region: APHRODITE_S3_REGION,
-      credentials: {
-        accessKeyId: APHRODITE_S3_ACCESS_KEY,
-        secretAccessKey: APHRODITE_S3_SECRET_KEY
-      }
-    },
-    aphroditeEndpoint: APHRODITE_ENDPOINT,
-    aphoditeConfig: {
-      model: './models/kuchiki-l2-7b',
-      temperature: 0.7,
-      max_tokens: 100,
-      top_p: 0.1,
-      frequency_penalty: 1.18,
-      presence_penalty: 0,
-      stop: ["</s>", "<|", "\n#", "\n*{{user}} ", "\n\n\n"],
-    },
-  })
 }
 
 export default app;
