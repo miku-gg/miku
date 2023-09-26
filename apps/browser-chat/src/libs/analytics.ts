512c7f647a1e1f4c5948ee703705c0d08a9d3366
mikudev <iwantofftheride@yahoo.com>
2023-09-26 10:51:54 -0300

    Fix events

--- apps/browser-chat/src/libs/analytics.ts
+++ apps/browser-chat/src/libs/analytics.ts
@@ -1,9 +1,9 @@
-// @ts-ignore fu
-const dataLayer = window.dataLayer || [];
-
 type TrackableEvent = 'bot_interact' | 'bot_regenerate'; 
 
 export const trackEvent = (event: TrackableEvent, data: any) => {
+  // @ts-ignore
+  const dataLayer = window.dataLayer || [];
+
   dataLayer.push({
     event,
     ...data
