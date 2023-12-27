6fdcb89b0dda3a39278731e1c4fde973e4357feb
mikudev <iwantofftheride@yahoo.com>
2023-12-27 12:51:39 -0300

    Clean up continue query

--- apps/interactor/src/libs/memory/strategies/RoleplayStrategyAlpaca.ts
+++ apps/interactor/src/libs/memory/strategies/RoleplayStrategyAlpaca.ts
@@ -17,8 +17,7 @@ export class RoleplayStrategyAlpaca extends AbstractPromptStrategy {
   override buildPrompt(
     state: RootState,
     maxNewTokens: number,
-    memorySize: number,
-    continueResponse?: boolean
+    memorySize: number
   ): {
     template: string
     variables: Record<string, string | string[]>
@@ -55,10 +54,6 @@ export class RoleplayStrategyAlpaca extends AbstractPromptStrategy {
     template += scenario ? `${scenario}\n` : ''
 
     template += this.getDialogueHistoryPrompt(state, memorySize)
-    if (continueResponse) {
-      template +=
-        "\n\n### Instruction: Continue writing {{char}}'s response below."
-    }
     template += this.getResponseAskLine(state, maxNewTokens)
 
     template = fillTextTemplate(template, {
