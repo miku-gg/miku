af659853b1d5678a6c179a2296b59ac62cef83f9
mikudev <iwantofftheride@yahoo.com>
2024-02-14 20:07:13 -0300

    Add ScenesPanel boilerplate

--- apps/bot-builder/src/panels/index.tsx
+++ apps/bot-builder/src/panels/index.tsx
@@ -1,10 +1,28 @@
 import { AreYouSure } from "@mikugg/ui-kit";
 import AssetsPanel from "./assets/AssetsPanel";
+import ButtonGroup from "../components/ButtonGroup";
+import { useState } from "react";
+import ScenesPanel from "./scenes/ScenesPanel";
 
 export default function App() {
+  const [selectedPanel, setSelectedPanel] = useState<string>("assets");
   return (
     <div className="app">
-      <AssetsPanel />
+      <ButtonGroup
+        buttons={[
+          {
+            text: "Assets",
+            value: "assets",
+          },
+          {
+            text: "Scenes",
+            value: "scenes",
+          },
+        ]}
+        selected={selectedPanel}
+        onButtonClick={(value) => setSelectedPanel(value)}
+      />
+      {selectedPanel === "assets" ? <AssetsPanel /> : <ScenesPanel />}
     </div>
   );
 }
---
+++ apps/bot-builder/src/panels/scenes/ScenesPanel.tsx
@@ -0,0 +1,12 @@
+import "./ScenesPanel.scss";
+
+export default function ScenesPanel() {
+  return (
+    <div className="AssetsPanel page-container">
+      <div className="AssetsPanel__header">
+        <div className="AssetsPanel__title title">Scenes</div>
+        <div className="AssetsPanel__description description">scenes panel</div>
+      </div>
+    </div>
+  );
+}
