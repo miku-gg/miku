307f4e693e867e5cf0d16653e60dd1699abd72d7
mikudev <iwantofftheride@yahoo.com>
2024-02-16 14:57:26 -0300

    Refactored SceneEditModal component to optimize state management and update delete scene functionality.

--- apps/bot-builder/src/modals/scene/SceneEditModal.tsx
+++ apps/bot-builder/src/modals/scene/SceneEditModal.tsx
@@ -1,25 +1,32 @@
 import { AreYouSure, Button, Input, Modal } from "@mikugg/ui-kit";
 import { useAppSelector, useAppDispatch } from "../../state/store";
-import { selectEditingScene, selectBackgrounds, selectEditingBackground, selectEditingCharacter } from "../../state/selectors";
-import { updateScene, deleteSceneById } from "../../state/slices/novelFormSlice";
-import { selectEditingScene } from "../../state/selectors";
-import { useDispatch } from "react-redux";
+import { selectEditingScene, selectBackgrounds } from "../../state/selectors";
+import {
+  updateScene,
+  deleteSceneById,
+} from "../../state/slices/novelFormSlice";
 import { closeModal } from "../../state/slices/inputSlice";
 import config from "../../config";
 import "./SceneEditModal.scss";
-import { useState, useEffect } from 'react';
-import { RiEdit2Line } from 'react-icons/ri';
+import { useState, useEffect } from "react";
+import { RiEdit2Line } from "react-icons/ri";
 
 export default function SceneEditModal() {
+  const dispatch = useAppDispatch();
+  const { openModal: openAreYouSure } = AreYouSure.useAreYouSure();
   const scene = useAppSelector(selectEditingScene);
   const backgrounds = useAppSelector(selectBackgrounds);
-  const characters = useAppSelector(selectEditingCharacter);
-  const dispatch = useAppDispatch();
-  const [sceneName, setSceneName] = useState(scene?.name || '');
-  const [scenePrompt, setScenePrompt] = useState(scene?.prompt || '');
-  const [selectedBackground, setSelectedBackground] = useState(scene?.background || backgrounds[0]);
-  const [selectedCharacters, setSelectedCharacters] = useState(scene?.characters || []);
-  const [selectedMusic, setSelectedMusic] = useState(scene?.music || '');
+  const characters = useAppSelector((state) => state.novel.characters);
+
+  const [sceneName, setSceneName] = useState(scene?.name || "");
+  const [scenePrompt, setScenePrompt] = useState(scene?.prompt || "");
+  const [selectedBackground, setSelectedBackground] = useState(
+    scene?.background || backgrounds[0]
+  );
+  const [selectedCharacters, setSelectedCharacters] = useState(
+    scene?.characters || []
+  );
+  const [selectedMusic, setSelectedMusic] = useState(scene?.music || "");
 
   useEffect(() => {
     if (scene) {
@@ -49,11 +56,15 @@ export default function SceneEditModal() {
 
   const handleDeleteScene = () => {
     if (scene) {
-      dispatch(deleteSceneById(scene.id));
-      dispatch(closeModal({ modalType: "scene" }));
+      openAreYouSure({
+        description: "Are you sure you what to delete this scene?",
+        onYes: () => {
+          dispatch(deleteSceneById(scene.id));
+          dispatch(closeModal({ modalType: "scene" }));
+        },
+      });
     }
   };
-  const dispatch = useDispatch();
 
   return (
     <Modal
@@ -65,15 +76,28 @@ export default function SceneEditModal() {
     >
       {scene ? (
         <div className="SceneEditModal_content">
-          <Input label="Scene Name" value={sceneName} onChange={handleSceneNameChange} />
+          <Input
+            label="Scene Name"
+            value={sceneName}
+            onChange={handleSceneNameChange}
+          />
           <div className="SceneEditModal_background">
-            <img src={config.genAssetLink(selectedBackground.source.jpg)} alt="Background" />
+            <img
+              src={config.genAssetLink(selectedBackground.source.jpg)}
+              alt="Background"
+            />
             <RiEdit2Line className="SceneEditModal_edit-icon" />
           </div>
           {/* TODO: Add character selection list */}
           {/* TODO: Add music selection */}
-          <Input label="Scene Prompt" value={scenePrompt} onChange={handleScenePromptChange} />
-          <Button theme="danger" onClick={handleDeleteScene}>Delete Scene</Button>
+          <Input
+            label="Scene Prompt"
+            value={scenePrompt}
+            onChange={handleScenePromptChange}
+          />
+          <Button theme="primary" onClick={handleDeleteScene}>
+            Delete Scene
+          </Button>
         </div>
       ) : null}
     </Modal>
