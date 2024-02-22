88deff8f40a37e7a46defce1c37015c72026b6e5
mikudev <iwantofftheride@yahoo.com>
2024-02-22 15:26:33 -0300

    Added importCard module for novel-builder app.

---
+++ apps/novel-builder/src/libs/importCard.ts
@@ -0,0 +1,6 @@
+import { TavernCardV2 } from "@mikugg/bot-utils";
+import { NovelFormState } from "../state/NovelFormState";
+
+export const tavernV2ToNovelState = (
+  tavernV2: TavernCardV2
+): NovelFormState => {};
