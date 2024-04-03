2fd5d8b8f2a78fa62ff2b425d01818e66fcea248
Mikudev <iwantofftheride@yahoo.com>
2024-04-03 18:08:40 -0300

    Add demo agent

---
+++ packages/guidance/src/lib/Agent.ts
@@ -0,0 +1,88 @@
+function replaceTemplate(template: string, key: string, value: string): string {
+  let regex = new RegExp(`{{GEN ${key}[^}]*}}`, "g");
+  template = template.replace(regex, value);
+  regex = new RegExp(`{{SEL ${key}[^}]*}}`, "g");
+  return template.replace(regex, value);
+}
+
+interface AgentConfiguration<
+  // extends type array of constant strings
+  AgentInputs extends string[],
+  AgentOutputs extends string[]
+> {
+  instruction: string;
+  description: string;
+  shotTemplate: {
+    input: string;
+    output: string;
+  };
+  shots: {
+    inputs: Record<AgentInputs[number], string>;
+    outputs: Record<AgentOutputs[number], string>;
+  }[];
+  instructSettings?: {
+    system?: string;
+    instruction?: string;
+    input?: string;
+    output?: string;
+  };
+}
+
+export class Agent<
+  AgentInputs extends string[],
+  AgentOutputs extends string[]
+> {
+  private configuration: AgentConfiguration<AgentInputs, AgentOutputs> & {
+    instructSettings: {
+      system: string;
+      instruction: string;
+      input: string;
+      output: string;
+    };
+  };
+
+  constructor(configuration: AgentConfiguration<AgentInputs, AgentOutputs>) {
+    this.configuration = {
+      ...configuration,
+      instructSettings: {
+        system: "",
+        instruction: "\n### Instruction:\n",
+        input: "\n### Input:\n",
+        output: "\n### Response:\n",
+        ...configuration.instructSettings,
+      },
+    };
+  }
+
+  public generatePrompt(inputs: Record<AgentInputs[number], string>): string {
+    let prompt = this.configuration.instructSettings.system;
+    prompt += this.configuration.description;
+    prompt += this.configuration.instructSettings.instruction;
+    prompt += this.configuration.instruction;
+    this.configuration.shots.forEach((shot) => {
+      prompt += this.configuration.instructSettings.input;
+      prompt += this.configuration.shotTemplate.input;
+      Object.keys(shot.inputs).forEach((key: AgentInputs[number]) => {
+        const value = shot.inputs[key];
+        prompt = prompt.replace(new RegExp(`{{${key}}}`, "g"), value as string);
+      });
+      prompt += this.configuration.instructSettings.output;
+      prompt += this.configuration.shotTemplate.output;
+      Object.keys(shot.outputs).forEach((key: AgentInputs[number]) => {
+        const value = shot.outputs[key];
+        prompt = prompt.replace(new RegExp(`{{${key}}}`, "g"), value);
+        prompt = replaceTemplate(prompt, key, value as string);
+      });
+    });
+    prompt += this.configuration.instructSettings.input;
+    prompt += this.configuration.shotTemplate.input;
+    Object.keys(inputs).forEach((key: AgentInputs[number]) => {
+      const value = inputs[key];
+      prompt = prompt.replace(new RegExp(`{{${key}}}`, "g"), value as string);
+    });
+    prompt += this.configuration.instructSettings.output;
+    prompt += this.configuration.shotTemplate.output;
+
+    return prompt;
+  }
+}
