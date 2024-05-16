672af576623929e81e74e564a93cd4bca97649a7
Lredigonda <leoredigonda@hotmail.com>
2024-05-16 15:22:02 -0300

    WIP add draggable and resizable chatbox

--- apps/interactor/src/components/chat-box/ChatBox.tsx
+++ apps/interactor/src/components/chat-box/ChatBox.tsx
@@ -1,12 +1,35 @@
+import { Rnd } from 'react-rnd'
+import { useAppContext } from '../../App.context'
+import './ChatBox.scss'
 import InputBox from './InputBox'
 import ResponseBox from './ResponseBox'
-import './ChatBox.scss'
 
 const ChatBox = (): JSX.Element | null => {
+  const { isMobileApp } = useAppContext()
+  const centeredPositionX = (window.innerWidth - window.innerWidth * 0.75) / 2
   return (
     <div className="ChatBox">
-      <ResponseBox />
-      <InputBox />
+      {isMobileApp ? (
+        <>
+          <ResponseBox />
+          <InputBox />
+        </>
+      ) : (
+        //eslint-disable-next-line
+        // @ts-ignore
+        <Rnd
+          className="ChatBox__Rnd"
+          default={{
+            x: window.innerWidth > 820 ? centeredPositionX : 0,
+            y: 0,
+            width: 'fit-content',
+            height: 'fit-content',
+          }}
+        >
+          <ResponseBox />
+          <InputBox />
+        </Rnd>
+      )}
     </div>
   )
 }
