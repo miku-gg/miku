cc8f072a161dda75ee06d049326820d7b324c8dc
Lredigonda <leoredigonda@hotmail.com>
2024-06-10 15:19:07 -0300

    add "jpg" and "jpeg" file types for upload avatar image

--- apps/novel-builder/src/libs/utils.ts
+++ apps/novel-builder/src/libs/utils.ts
@@ -20,7 +20,10 @@ export const hashBase64URI = async (base64Content: string): Promise<string> => {
   return hashBase64(base64Content.split(",")[1]);
 };
 
-export const checkFileType = (file: File, types = ["image/png"]): boolean => {
+export const checkFileType = (
+  file: File,
+  types = ["image/png", "image/jpeg", "image/jpg"]
+): boolean => {
   return types.includes(file.type);
 };
 
