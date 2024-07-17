0126809e4004f25ac3cc91e0ff125319232efa72
mikudev <iwantofftheride@yahoo.com>
2024-07-17 18:42:19 -0300

    Add no data message for empty debug

--- apps/interactor/src/components/interactor/DebugModal.tsx
+++ apps/interactor/src/components/interactor/DebugModal.tsx
@@ -28,59 +28,70 @@ export default function DebugModal() {
       className="DebugModal__container"
     >
       <div className="DebugModal">
-        <div className="DebugModal__items scrollbar">
-          {completionHistory.map((item, index) => (
-            <Button
-              theme={
-                index === completionHistoryIndex ? 'secondary' : 'transparent'
-              }
-              key={index}
-              className="DebugModal__item"
-              onClick={() => setCompletionHistoryIndex(index)}
-            >
-              {new Date(item.timestamp).toLocaleTimeString()}
-            </Button>
-          ))}
-        </div>
-        {completionHistory[completionHistoryIndex] ? (
-          <div className="DebugModal__content">
-            <div className="DebugModal__stats scrollbar">
-              {JSON.stringify(
-                {
-                  model: completionHistory[completionHistoryIndex].model,
-                  variables:
-                    completionHistory[completionHistoryIndex].variables,
-                },
-                null,
-                2
-              )}
+        {completionHistory.length === 0 ? (
+          <div className="DebugModal__stats">No data</div>
+        ) : (
+          <>
+            <div className="DebugModal__items scrollbar">
+              {completionHistory.map((item, index) => (
+                <Button
+                  theme={
+                    index === completionHistoryIndex
+                      ? 'secondary'
+                      : 'transparent'
+                  }
+                  key={index}
+                  className="DebugModal__item"
+                  onClick={() => setCompletionHistoryIndex(index)}
+                >
+                  {new Date(item.timestamp).toLocaleTimeString()}
+                </Button>
+              ))}
             </div>
-            <div className="DebugModal__template scrollbar">
-              {completionHistory[completionHistoryIndex].template
-                .split('\n')
-                .map((value, index) => {
-                  const systemLine = ['###', 'USER:', 'ASSISTANT:', '<|'].some(
-                    (v) => value.startsWith(v)
-                  )
-                  const genLine =
-                    value.includes('{{SEL') || value.includes('{{GEN')
+            {completionHistory[completionHistoryIndex] ? (
+              <div className="DebugModal__content">
+                <div className="DebugModal__stats scrollbar">
+                  {JSON.stringify(
+                    {
+                      model: completionHistory[completionHistoryIndex].model,
+                      variables:
+                        completionHistory[completionHistoryIndex].variables,
+                    },
+                    null,
+                    2
+                  )}
+                </div>
+                <div className="DebugModal__template scrollbar">
+                  {completionHistory[completionHistoryIndex].template
+                    .split('\n')
+                    .map((value, index) => {
+                      const systemLine = [
+                        '###',
+                        'USER:',
+                        'ASSISTANT:',
+                        '<|',
+                      ].some((v) => value.startsWith(v))
+                      const genLine =
+                        value.includes('{{SEL') || value.includes('{{GEN')
 
-                  return (
-                    <div
-                      key={`${index}-${value}`}
-                      className={classNames({
-                        DebugModal__template__line: true,
-                        'DebugModal__template__line--system': systemLine,
-                        'DebugModal__template__line--gen': genLine,
-                      })}
-                    >
-                      {value}
-                    </div>
-                  )
-                })}
-            </div>
-          </div>
-        ) : null}
+                      return (
+                        <div
+                          key={`${index}-${value}`}
+                          className={classNames({
+                            DebugModal__template__line: true,
+                            'DebugModal__template__line--system': systemLine,
+                            'DebugModal__template__line--gen': genLine,
+                          })}
+                        >
+                          {value}
+                        </div>
+                      )
+                    })}
+                </div>
+              </div>
+            ) : null}
+          </>
+        )}
       </div>
     </Modal>
   )
