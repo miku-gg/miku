254d3eca039e4efa451c5ea18b1822e1216e7865
mikudev <iwantofftheride@yahoo.com>
2024-07-31 21:44:01 -0300

    Add mistral prompt strategy

--- apps/interactor/src/libs/prompts/strategies/roleplay/index.ts
+++ apps/interactor/src/libs/prompts/strategies/roleplay/index.ts
@@ -4,37 +4,31 @@ import {
   RoleplayStrategyMetharme,
   RoleplayStrategyLlama3,
   RoleplayStrategyVicuna,
-} from '..'
+} from '..';
+import { RoleplayStrategyMistral } from './RoleplayStrategyMistral';
 
-export const roleplayStrategySlugs = [
-  'alpacarp',
-  'metharmerp',
-  'vicunarp',
-  'llama3rp',
-] as const
-export type RoleplayStrategySlug = (typeof roleplayStrategySlugs)[number]
-export function isOfTypeStrategySlug(
-  slug: string | undefined
-): slug is RoleplayStrategySlug {
-  return typeof slug === typeof undefined
-    ? false
-    : (roleplayStrategySlugs as readonly string[]).includes(slug!)
+export const roleplayStrategySlugs = ['alpacarp', 'metharmerp', 'mistralrp', 'vicunarp', 'llama3rp'] as const;
+export type RoleplayStrategySlug = (typeof roleplayStrategySlugs)[number];
+export function isOfTypeStrategySlug(slug: string | undefined): slug is RoleplayStrategySlug {
+  return typeof slug === typeof undefined ? false : (roleplayStrategySlugs as readonly string[]).includes(slug!);
 }
 
 export const getRoleplayStrategyFromSlug = (
   slug: RoleplayStrategySlug,
-  tokenizerSlug: string = 'llama'
+  tokenizerSlug: string = 'llama',
 ): AbstractRoleplayStrategy => {
   switch (slug) {
     case 'alpacarp':
-      return new RoleplayStrategyAlpaca(tokenizerSlug)
+      return new RoleplayStrategyAlpaca(tokenizerSlug);
     case 'metharmerp':
-      return new RoleplayStrategyMetharme(tokenizerSlug)
+      return new RoleplayStrategyMetharme(tokenizerSlug);
     case 'llama3rp':
-      return new RoleplayStrategyLlama3(tokenizerSlug)
+      return new RoleplayStrategyLlama3(tokenizerSlug);
     case 'vicunarp':
-      return new RoleplayStrategyVicuna(tokenizerSlug)
+      return new RoleplayStrategyVicuna(tokenizerSlug);
+    case 'mistralrp':
+      return new RoleplayStrategyMistral(tokenizerSlug);
     default:
-      throw new Error(`Invalid roleplay strategy slug: ${slug}`)
+      throw new Error(`Invalid roleplay strategy slug: ${slug}`);
   }
-}
+};
