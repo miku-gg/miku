5ec636745d0a256ff7a66a9004e2173ded7675b7
Lredigonda <leoredigonda@hotmail.com>
2024-08-15 12:40:25 -0300

    make requested changes

--- apps/interactor/src/components/interactor/PremiumHint.tsx
+++ apps/interactor/src/components/interactor/PremiumHint.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import { GoAlertFill } from 'react-icons/go';
+import { useState } from 'react';
+import { GiBrain } from 'react-icons/gi';
 import { selectChatHistory } from '../../state/selectors';
 import { useAppSelector } from '../../state/store';
 import './PremiumHint.scss';
 
-const PREMIUM_HINT = `Characters' memory is degrading. Upgrade to premium to have enhanced memory.`;
+const PREMIUM_HINT = `Characters will forget old messages. Upgrade to premium for longer memory.`;
 const DEFAULT_MESSAGES_COUNT = 15;
 
 export default function PremiumHint() {
@@ -13,19 +13,9 @@ export default function PremiumHint() {
   const chatHistory = useAppSelector(selectChatHistory);
   const interactionsCount = chatHistory.filter((message) => message.type === 'interaction').length;
 
-  const speed = 5;
-  const position = Math.max(PREMIUM_HINT.length + 10, 20);
-  const animationDuration = Math.max(PREMIUM_HINT.length / speed, 3);
-
   if (interactionsCount < DEFAULT_MESSAGES_COUNT || isPremiumUser) {
     return null;
   }
-
-  useEffect(() => {
-    if (interactionsCount >= DEFAULT_MESSAGES_COUNT && interactionsCount % 2 === 0) {
-      setHidden(false);
-    }
-  }, [interactionsCount]);
 
   return (
     <div
@@ -36,22 +26,11 @@ export default function PremiumHint() {
     >
       <div className="PremiumHint__content">
         <div className="PremiumHint__icon">
-          <GoAlertFill />
+          <GiBrain />
         </div>
         <div className="PremiumHint__text">
           <span className="Hint__text-span">
-            <span
-              className="PremiumHint__text-label"
-              style={
-                {
-                  '--initial-text-position': `0`,
-                  '--ending-text-position': `${-position}ch`,
-                  '--animation-duration': `${animationDuration}s`,
-                } as React.CSSProperties
-              }
-            >
-              {PREMIUM_HINT}
-            </span>
+            <span className="PremiumHint__text-label">{PREMIUM_HINT}</span>
           </span>
         </div>
       </div>
