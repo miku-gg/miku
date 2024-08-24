503bb8eeb90b7853e44b1067574f26939c4851ef
Lredigonda <leoredigonda@hotmail.com>
2024-08-24 16:52:46 +0300

    add generate scene in mobile app

--- apps/interactor/src/components/scenarios/SceneSelector.tsx
+++ apps/interactor/src/components/scenarios/SceneSelector.tsx
@@ -103,41 +103,40 @@ export default function SceneSelector(): JSX.Element | null {
                 <div className="SceneSelector__item-background" style={{ backgroundColor: 'gray' }} />
                 <div className="SceneSelector__item-text">Create new scene</div>
               </button>
-              {!isMobileApp && (
-                <button
-                  className="SceneSelector__item"
-                  onClick={() => {
-                    if (isInteractionDisabled) {
-                      toast.warn('Please log in to interact.', {
-                        position: 'top-center',
-                        style: {
-                          top: 10,
-                        },
-                      });
-                      return;
-                    }
-                    dispatch(
-                      setModalOpened({
-                        id: 'scene-suggestions',
-                        opened: true,
-                      }),
-                    );
-                    dispatch(
-                      userDataFetchStart({
-                        apiEndpoint,
-                      }),
-                    );
-                    trackEvent('scene-generate');
-                  }}
-                >
-                  <div className="SceneSelector__item-background SceneSelector__item-background--aero">
-                    <StarsEffect />
-                  </div>
-                  <div className="SceneSelector__item-text">
-                    Generate Scene <BsStars />
-                  </div>
-                </button>
-              )}
+
+              <button
+                className="SceneSelector__item"
+                onClick={() => {
+                  if (isInteractionDisabled) {
+                    toast.warn('Please log in to interact.', {
+                      position: 'top-center',
+                      style: {
+                        top: 10,
+                      },
+                    });
+                    return;
+                  }
+                  dispatch(
+                    setModalOpened({
+                      id: 'scene-suggestions',
+                      opened: true,
+                    }),
+                  );
+                  dispatch(
+                    userDataFetchStart({
+                      apiEndpoint,
+                    }),
+                  );
+                  trackEvent('scene-generate');
+                }}
+              >
+                <div className="SceneSelector__item-background SceneSelector__item-background--aero">
+                  <StarsEffect />
+                </div>
+                <div className="SceneSelector__item-text">
+                  Generate Scene <BsStars />
+                </div>
+              </button>
             </div>
           )}
         </div>
