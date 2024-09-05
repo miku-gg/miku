7e094118de1c63f6caa32576e3f231216663061b
LautaroCesso <lautaro_cesso@hotmail.com>
2024-09-06 00:04:45 +0700

    Fix

--- apps/interactor/src/components/interactor/InteractorHeader.tsx
+++ apps/interactor/src/components/interactor/InteractorHeader.tsx
@@ -10,7 +10,7 @@ import Hint from './Hint';
 import './InteractorHeader.scss';
 import { InventoryTrigger } from './Inventory';
 import MemoryCapacityView from './MemoryCapacityView';
-import ModelSelector from './ModelSelector';
+// import ModelSelector from './ModelSelector';
 import MusicPlayer from './MusicPlayer';
 import PremiumHint from './PremiumHint';
 import ScreenSizer from './ScreenSizer';
