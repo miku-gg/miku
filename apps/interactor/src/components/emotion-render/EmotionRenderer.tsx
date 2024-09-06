afa51c98ec90bb8ebefdcf436d7882106b61ea08
Mikudev <iwantofftheride@yahoo.com>
2024-09-06 21:49:59 +0700

    Revert "Use direct link for EmotionRenderer"
    This reverts commit f7c3aa284a8965609a19d5b5560d17e4c4f5c900.

--- apps/interactor/src/components/emotion-render/EmotionRenderer.tsx
+++ apps/interactor/src/components/emotion-render/EmotionRenderer.tsx
@@ -1,4 +1,13 @@
+import { useEffect, useRef, useState } from 'react';
 import './EmotionRenderer.scss';
+
+const assets = new Map<
+  string,
+  {
+    url: string;
+    fileType: string | null;
+  }
+>();
 
 export default function EmotionRenderer({
   assetUrl,
@@ -11,11 +20,67 @@ export default function EmotionRenderer({
   upDownAnimation?: boolean;
   assetLinkLoader: (asset: string, lowres?: boolean) => string;
 }): JSX.Element {
-  if (assetUrl.endsWith('.webm')) {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [currentAsset, setCurrentAsset] = useState<{
+    url: string;
+    fileType: string | null;
+  }>({
+    url: '',
+    fileType: null,
+  });
+  const assetVersion = useRef(0); // To keep track of asset versions
+
+  useEffect(() => {
+    const currentVersion = ++assetVersion.current;
+    const controller = new AbortController();
+    async function fetchAndSetAsset(url: string, lowres?: boolean) {
+      const response = await fetch(assetLinkLoader(url, lowres));
+      if (response.ok && response.status === 200) {
+        const contentType = response.headers.get('Content-Type');
+        const data = await response.blob();
+        const newBlobUrl = URL.createObjectURL(data);
+        assets.set(url, {
+          fileType: contentType,
+          url: newBlobUrl,
+        });
+        if (currentVersion !== assetVersion.current) {
+          return;
+        }
+        setCurrentAsset({ fileType: contentType, url: newBlobUrl });
+      }
+    }
+
+    async function fetchAssets() {
+      setLoading(true);
+
+      await fetchAndSetAsset(assetUrl, true);
+
+      fetchAndSetAsset(assetUrl).then(() => {
+        setLoading(false);
+      });
+    }
+
+    // Fetch assets only if they haven't been fetched before or if assetUrl changes.
+    if (!assets.has(assetUrl)) {
+      fetchAssets();
+    } else {
+      // If we already have the asset, use it and don't show the loading state.
+      setCurrentAsset(assets.get(assetUrl) || { url: '', fileType: null });
+      setLoading(false);
+    }
+
+    return () => {
+      controller.abort(); // Cancel the fetch operation if the component unmounts or the assetUrl changes
+    };
+  }, [assetUrl, assetLinkLoader]);
+
+  const { url: blobUrl, fileType } = currentAsset;
+
+  if (fileType === 'video/webm') {
     return (
       <video
-        className={`${className} ${upDownAnimation ? 'fade-in up-and-down' : ''}`}
-        src={assetLinkLoader(assetUrl)}
+        className={`${className} ${!loading && upDownAnimation ? 'fade-in up-and-down' : ''}`}
+        src={blobUrl}
         loop
         autoPlay
         muted
@@ -24,8 +89,8 @@ export default function EmotionRenderer({
   } else {
     return (
       <img
-        className={`${className} ${upDownAnimation ? 'fade-in up-and-down' : ''}`}
-        src={assetLinkLoader(assetUrl)}
+        className={`${className} ${!loading && upDownAnimation ? 'fade-in up-and-down' : ''}`}
+        src={blobUrl}
         alt="character"
         onError={({ currentTarget }) => {
           currentTarget.onerror = null;
