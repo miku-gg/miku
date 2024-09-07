8c3cc3e8c126300c27dd15941aa2e83572c9399a
Lredigonda <leoredigonda@hotmail.com>
2024-09-07 12:19:19 +0700

    wip

--- apps/interactor/src/components/novel-loader/NovelLoader.tsx
+++ apps/interactor/src/components/novel-loader/NovelLoader.tsx
@@ -1,16 +1,19 @@
 import { EMOTION_GROUP_TEMPLATES } from '@mikugg/bot-utils';
 import { Modal } from '@mikugg/ui-kit';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppContext } from '../../App.context';
 import { registerTrackSessionData } from '../../libs/analytics';
 import { replaceState } from '../../state/slices/replaceState';
 import { setName, setSystemPrompt } from '../../state/slices/settingsSlice';
 import { RootState, useAppDispatch, useAppSelector } from '../../state/store';
+import { NovelNSFW } from '../../state/versioning';
 import { Loader } from '../common/Loader';
 import './NovelLoader.scss';
+import { start } from 'repl';
 
 const NovelLoader = (): JSX.Element => {
   const { novelLoader, persona, isMobileApp } = useAppContext();
+  const [completelyNSFW, setCompletelyNSFW] = useState(false);
   const novelFetching = useAppSelector((state) => !state.novel.starts.length);
   const dispatch = useAppDispatch();
 
@@ -30,6 +33,58 @@ const NovelLoader = (): JSX.Element => {
           }
         });
       });
+      console.log('previous state: ', state);
+
+      if (state.settings.user.nsfw === NovelNSFW.NONE) {
+        let starts = state.novel.starts;
+        starts = starts.filter((start) => {
+          return state.novel.scenes.find((scene) => scene.id === start.sceneId)?.nsfw === NovelNSFW.NONE;
+        })
+        state.novel.scenes = state.novel.scenes.filter((scene) => scene.nsfw === NovelNSFW.NONE);
+        state.novel.characters = state.novel.characters.filter((character) => character.nsfw === NovelNSFW.NONE);
+        const sceneIds = state.novel.scenes.map((scene) => scene.id);
+        state.novel.starts = state.novel.starts.filter((start) => sceneIds.includes(start.sceneId));
+        const interactionSceneIds = Object.values(state.narration.interactions).map((interaction) => {
+          return interaction ? interaction.sceneId : '';
+        });
+        //remove all interactions that are not in the sceneIds
+        Object.keys(state.narration.interactions).forEach((key) => {
+          if (!sceneIds.includes(key) && !interactionSceneIds.includes(key)) {
+            delete state.narration.interactions[key];
+          }
+        });
+
+        const interactionResponseIds = Object.values(state.narration.interactions)
+          .map((interaction) => {
+            return interaction ? interaction.responsesId : null;
+          })
+          .flat()
+          .filter((responseId) => responseId !== null);
+
+        if (interactionResponseIds.length > 0) {
+          Object.keys(state.narration.responses).forEach((key) => {
+            if (!interactionResponseIds.includes(key)) {
+              delete state.narration.responses[key];
+            }
+          });
+        } else {
+          Object.values(state.narration.responses).forEach((response) => {
+            if (response && response.parentInteractionId === null) {
+              if (!state.novel.starts.map((start) => start.id).includes(response.id)) {
+                delete state.narration.responses[response.id];
+              }
+            }
+          });
+          state.narration.currentResponseId = state.novel.starts[0].id;
+        }
+
+        if (starts.length === 0 || state.novel.scenes.length === 0 || state.novel.characters.length === 0) {
+          setCompletelyNSFW(true);
+          return new Error('This novel is NSFW.');
+        }
+      }
+      console.log('Modified state: ', state);
+
       dispatch(replaceState(state));
       if (persona?.name) {
         if (persona?.description) {
@@ -48,10 +103,8 @@ const NovelLoader = (): JSX.Element => {
   return (
     <Modal className={isMobileApp ? 'NovelLoaderModal' : ''} opened={novelFetching}>
       <div className="NovelLoader">
-        <div className="NovelLoader__text">Loading Novel</div>
-        <div className="NovelLoader__loader">
-          <Loader />
-        </div>
+        <div className="NovelLoader__text">{completelyNSFW ? 'This novel is NSFW.' : 'Loading Novel'}</div>
+        <div className="NovelLoader__loader">{!completelyNSFW && <Loader />}</div>
       </div>
     </Modal>
   );
