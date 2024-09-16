5af7f39f4fc78981f7b6791b92f3b72235db95a2
Lredigonda <leoredigonda@hotmail.com>
2024-09-16 14:36:35 +0700

    hot fix

--- apps/interactor/src/components/history/History.tsx
+++ apps/interactor/src/components/history/History.tsx
@@ -35,8 +35,6 @@ const HistoryActions = () => {
   const state = useAppSelector((state) => state);
   const hasInteractions = useAppSelector((state) => Object.keys(state.narration.interactions).length > 0);
 
-  const { isMobileApp } = useAppContext();
-
   const handleSave = () => {
     const clonedState = JSON.parse(JSON.stringify(state));
     clonedState.settings.modals.history = false;
