066f5f965d21421809b3b56629298fff84d5142d
Lredigonda <leoredigonda@hotmail.com>
2024-10-24 11:36:06 +0900

    add styles in cutscenes parts renderer

--- apps/novel-builder/src/modals/cutscenes/CutscenesPartsRender.tsx
+++ apps/novel-builder/src/modals/cutscenes/CutscenesPartsRender.tsx
@@ -1,19 +1,22 @@
 import { v4 as randomUUID } from 'uuid';
-import { AssetDisplayPrefix, NovelV3 } from '@mikugg/bot-utils';
+import { AssetDisplayPrefix } from '@mikugg/bot-utils';
 import { Button } from '@mikugg/ui-kit';
 import { selectEditingCutscene } from '../../state/selectors';
-import { createCutscenePart, updateCutscenePart } from '../../state/slices/novelFormSlice';
+import { createCutscenePart } from '../../state/slices/novelFormSlice';
 import { useAppDispatch, useAppSelector } from '../../state/store';
 import { openModal } from '../../state/slices/inputSlice';
 import './CutscenesPartsRender.scss';
-import { FaPencilAlt, FaTrashAlt } from 'react-icons/fa';
+import { FaPencilAlt } from 'react-icons/fa';
 import config from '../../config';
 import { PartEditModal } from './PartEditModal';
 
-export const CutScenePartsRender = ({ onDeletePart }: { onDeletePart: (id: string) => void }) => {
+export const CutScenePartsRender = () => {
   const dispatch = useAppDispatch();
   const currentCutscene = useAppSelector(selectEditingCutscene);
   const parts = currentCutscene?.parts || [];
+  const backgrounds = useAppSelector((state) => state.novel.backgrounds);
+  const characters = useAppSelector((state) => state.novel.characters);
+  const songs = useAppSelector((state) => state.novel.songs);
 
   const handleCreatePart = () => {
     if (!currentCutscene) return;
@@ -22,9 +25,30 @@ export const CutScenePartsRender = ({ onDeletePart }: { onDeletePart: (id: strin
     dispatch(openModal({ modalType: 'cutscenePartEdit', editId: id }));
   };
 
-  const updatePart = (part: NovelV3.CutScenePart) => {
-    if (!currentCutscene) return;
-    dispatch(updateCutscenePart({ cutsceneId: currentCutscene.id, part }));
+  const getBackgroundAssetSRC = (backgroundId: string) => {
+    const background = backgrounds.find((b) => b.id === backgroundId);
+    const src = background?.source.mp4 ? background.source.mp4 : background?.source.jpg;
+    if (!src) return '';
+    if (background?.source.mp4) {
+      return config.genAssetLink(src, AssetDisplayPrefix.BACKGROUND_VIDEO);
+    }
+    return config.genAssetLink(src, AssetDisplayPrefix.BACKGROUND_IMAGE_SMALL);
+  };
+
+  const getCharacterAssetSRC = (character: { id: string; outfitId: string; emotionId: string }) => {
+    const characterData = characters.find((c) => c.id === character.id);
+    if (!characterData) return '';
+    const outfit = characterData.card.data.extensions.mikugg_v2.outfits.find((o) => o.id === character.outfitId);
+    if (!outfit) return '';
+    const emotion = outfit.emotions.find((e) => e.id === character.emotionId);
+    if (!emotion) return '';
+    return config.genAssetLink(emotion?.sources.png, AssetDisplayPrefix.EMOTION_IMAGE_SMALL);
+  };
+
+  const getMusicAssetSRC = (musicId: string) => {
+    const song = songs.find((s) => s.id === musicId);
+    if (!song) return '';
+    return config.genAssetLink(song.source, AssetDisplayPrefix.MUSIC);
   };
 
   return (
@@ -36,38 +60,39 @@ export const CutScenePartsRender = ({ onDeletePart }: { onDeletePart: (id: strin
         </Button>
       </div>
       <div className="CutScenePartsRender__container">
-        {parts.length === 0 && <p>No parts yet</p>}
         {parts.map((part) => (
           <div key={part.id} className="CutScenePartsRender__part">
             <div className="CutScenePartsRender__part__buttons">
               <FaPencilAlt
+                className="CutScenePartsRender__part__buttons__icon"
                 onClick={() => {
-                  updatePart(part);
+                  dispatch(openModal({ modalType: 'cutscenePartEdit', editId: part.id }));
                 }}
               />
-              <FaTrashAlt
-                onClick={() => {
-                  onDeletePart(part.id);
-                }}
-              />
-            </div>
-            <div className="CutScenePartsRender__part__content">
-              <h3>{part.text}</h3>
-              <p>{part.type}</p>
-            </div>
-            <div className="CutScenePartsRender__part__music">
-              <audio src={config.genAssetLink(part.music, AssetDisplayPrefix.MUSIC)} controls />
             </div>
             <div className="CutScenePartsRender__part__assets">
-              <img src={config.genAssetLink(part.background, AssetDisplayPrefix.BACKGROUND_IMAGE_SMALL)} />
+              <h3>{part.type === 'dialogue' ? 'Dialogue' : 'Description'}</h3>
+              <img
+                className="CutScenePartsRender__part__assets__background"
+                src={getBackgroundAssetSRC(part.background)}
+              />
               {part.characters.map((character) => {
                 return (
                   <img
+                    className="CutScenePartsRender__part__assets__character"
                     key={character.id}
-                    src={config.genAssetLink(character.outfitId, AssetDisplayPrefix.ITEM_IMAGE_SMALL)}
+                    src={getCharacterAssetSRC(character)}
                   />
                 );
               })}
+            </div>
+
+            <div className="CutScenePartsRender__part__content">
+              <p>{part.text}</p>
+            </div>
+            <div className="CutScenePartsRender__part__music">
+              <h3>Music</h3>
+              <audio src={getMusicAssetSRC(part.music)} controls />
             </div>
           </div>
         ))}
