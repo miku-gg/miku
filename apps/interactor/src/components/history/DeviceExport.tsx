5e8de44a3b9712f3f1e42278a9259aaa96893685
Mikudev <iwantofftheride@yahoo.com>
2024-10-29 17:54:19 +0900

    fix encryption function

--- apps/interactor/src/components/history/DeviceExport.tsx
+++ apps/interactor/src/components/history/DeviceExport.tsx
@@ -17,43 +17,7 @@ import { uploadNarration } from '../../libs/platformAPI';
 import { CustomEventType, postMessage } from '../../libs/stateEvents';
 
 function stringToBase64(str: string): string {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(str);
-  let base64 = '';
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-  const bytes = new Uint8Array(data);
-  const byteLength = bytes.byteLength;
-  const byteRemainder = byteLength % 3;
-  const mainLength = byteLength - byteRemainder;
-
-  let a, b, c, d;
-  let chunk;
-
-  // Main loop deals with bytes in chunks of 3
-  for (let i = 0; i < mainLength; i = i + 3) {
-    chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
-    a = (chunk & 16515072) >> 18;
-    b = (chunk & 258048) >> 12;
-    c = (chunk & 4032) >> 6;
-    d = chunk & 63;
-    base64 += chars[a] + chars[b] + chars[c] + chars[d];
-  }
-
-  // Remaining bytes and padding
-  if (byteRemainder == 1) {
-    chunk = bytes[mainLength];
-    a = (chunk & 252) >> 2;
-    b = (chunk & 3) << 4;
-    base64 += chars[a] + chars[b] + '==';
-  } else if (byteRemainder == 2) {
-    chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
-    a = (chunk & 64512) >> 10;
-    b = (chunk & 1008) >> 4;
-    c = (chunk & 15) << 2;
-    base64 += chars[a] + chars[b] + chars[c] + '=';
-  }
-
-  return base64;
+  return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(str));
 }
 
 interface QRProps {
