82188da20526cc2119f10c68e3182531a2af67c6
mikudev <iwantofftheride@yahoo.com>
2025-09-30 22:33:58 -0300

    remove rnd for inner thoughts

--- apps/interactor/src/components/inner-thoughts/InnerThoughtsBox.tsx
+++ apps/interactor/src/components/inner-thoughts/InnerThoughtsBox.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Rnd } from 'react-rnd';
 import TTSPlayer from '../chat-box/TTSPlayer';
-import { IoIosCloseCircleOutline } from "react-icons/io";
+import { IoIosCloseCircleOutline } from 'react-icons/io';
 import './InnerThoughtsBox.scss';
-import { useAppContext } from '../../App.context';
 
 interface InnerThoughtsBoxProps {
   isVisible: boolean;
@@ -12,15 +10,8 @@ interface InnerThoughtsBoxProps {
   className?: string;
 }
 
-const InnerThoughtsBox: React.FC<InnerThoughtsBoxProps> = ({
-  isVisible,
-  thoughts,
-  onClose,
-  className = '',
-}) => {
+const InnerThoughtsBox: React.FC<InnerThoughtsBoxProps> = ({ isVisible, thoughts, onClose, className = '' }) => {
   if (!isVisible) return null;
-
-  const { isMobileApp } = useAppContext();
 
   return (
     <div className={`InnerThoughtsBox ${className}`}>
@@ -28,41 +19,20 @@ const InnerThoughtsBox: React.FC<InnerThoughtsBoxProps> = ({
         <div className="InnerThoughtsBox__header">
           <TTSPlayer text={thoughts || undefined} />
           {onClose && (
-            <IoIosCloseCircleOutline 
+            <IoIosCloseCircleOutline
               className="InnerThoughtsBox__close-button"
               onClick={onClose}
               aria-label="Close inner thoughts"
             />
           )}
         </div>
-        <div className='InnerThoughtsBox__body'>
-          <div className='InnerThoughtsBox__bg'></div>
+        <div className="InnerThoughtsBox__body">
+          <div className="InnerThoughtsBox__bg"></div>
           <div className="InnerThoughtsBox__content">
             {!thoughts ? (
               <p className="InnerThoughtsBox__empty">No thoughts generated.</p>
             ) : (
-              isMobileApp || window.innerWidth < 1024 ? (
-                <Rnd
-                  className="InnerThoughtsBox__rnd"
-                  bounds=".InnerThoughtsBox__content"
-                  enableResizing={false}
-                  dragAxis="y"
-                  default={{
-                    x: 0,
-                    y: 0,
-                    width: 'unset',
-                    height: 120, 
-                  }}
-                >
-                  <div className="InnerThoughtsBox__thoughts-text">
-                    {thoughts}
-                  </div>
-                </Rnd>
-              ) : (
-                <div className="InnerThoughtsBox__thoughts-text">
-                  {thoughts}
-                </div>
-              )
+              <div className="InnerThoughtsBox__thoughts-text">{thoughts}</div>
             )}
           </div>
         </div>
